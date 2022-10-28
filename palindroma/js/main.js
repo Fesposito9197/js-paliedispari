df981b302892bc546d8170a14c0e43e871f27811
Fesposito9197 <kekkoesp8@gmail.com>
2022-10-28 17:36:34 +0200

    palindromo built in function

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -6,6 +6,7 @@ const text = prompt('inserisci una parola');
 
 // creo una funzione che mi inverta la parola inserita dall'utente 
 function palindroma(textToCheck) {
+    
     let invertedCheck = "";
     for (let i = textToCheck.length - 1; i >= 0; i--) {
         invertedCheck += textToCheck[i]
@@ -17,11 +18,23 @@ function palindroma(textToCheck) {
         return false
     }
 }
-// richiamo la funzione dove se l'utente inserisce una parola palindroma da true invece da false in  caso contrario 
+function reverseString(textToRevert) {
+    console.log(textToRevert.split(""))
+    console.log(textToRevert.split("").reverse())
+    console.log(textToRevert.split("").reverse().join(""))
 
-if(palindroma(text) === true ){
+    return textToRevert.split("").reverse().join("");
+    
+}
+// richiamo la funzione dove se l'utente inserisce una parola palindroma da true invece da false in  caso contrario 
+if(reverseString(text) === text ){
     alert("Hai inserito una parola palindroma")
 }else{
     alert("Non hai inserito una parola palindroma")
 }
-console.log(palindroma(text))
+// if(palindroma(text) === true ){
+//     alert("Hai inserito una parola palindroma")
+// }else{
+//     alert("Non hai inserito una parola palindroma")
+// }
+// console.log(palindroma(text))
